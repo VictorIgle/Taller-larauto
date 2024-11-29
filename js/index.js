5838331989c30595de257fa914303e5e77d715df
VictorIgle <165699685+VictorIgle@users.noreply.github.com>
2024-11-29 16:01:31 +0100

    cambios css
    slider terminado

--- js/index.js
+++ js/index.js
@@ -1,1 +1,44 @@
 'use strict'
+
+
+const sliderNext = document.querySelector('.Slider-arrow--next')
+const sliderPrev = document.querySelector('.Slider-arrow--prev')
+const sliderBtn = document.querySelectorAll('.Slider-btn')
+const sliderImg = document.querySelectorAll('.Slider-img')
+
+
+
+let img = 0
+
+let imagenVisible = ()=>{
+    sliderImg.forEach(( eachImg , i)=>{
+        sliderImg[i].classList.remove('isVisible')
+        sliderBtn[i].classList.remove('isActive')
+    })
+
+        sliderImg[img].classList.add('isVisible')
+        sliderBtn[img].classList.add('isActive')
+}
+
+sliderNext.addEventListener(`click` , ()=>{
+    img++
+    if( img >= 5){
+        img = 0
+    }
+    imagenVisible()
+})
+
+sliderPrev.addEventListener(`click` , ()=>{
+    img--
+    if(img < 0 ){
+        img = 4
+    }
+    imagenVisible()
+})
+
+sliderBtn.forEach( (i)=>{
+    sliderBtn[i].addEventListener(`click` , ()=>{
+        img = i
+        imagenVisible()
+    })
+})
